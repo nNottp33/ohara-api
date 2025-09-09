f0a0caec11518e274af40574ad5099addcfb01fe
nnottp <nnattaponp.dev@gmail.com>
2025-09-09 10:25:23 +0700

    Integrate Carbon library into LoggerMiddleware for enhanced date-time formatting and update dependencies.

--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/dromara/carbon/v2 v2.6.9 // indirect
+	github.com/golang-module/carbon/v2 v2.6.9 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- internal/adapters/http/middleware/logger_middleware.go
+++ internal/adapters/http/middleware/logger_middleware.go
@@ -5,23 +5,26 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-module/carbon/v2"
 )
 
 func LoggerMiddleware(c *fiber.Ctx) error {
 	start := time.Now()
 	err := c.Next()
 	latency := time.Since(start)
-	ms := float64(latency) / float64(time.Millisecond)
+
+	now := carbon.Now().ToDateTimeString()
 
 	fmt.Printf(
 		"%s | %d | %.3f ms | %s | %s | %s | %s\n",
-		time.Now().Format("2006-01-02 15:04:05"),
+		now,
 		c.Response().StatusCode(),
-		ms,
+		float64(latency)/float64(time.Millisecond),
 		c.IP(),
 		c.Method(),
 		c.Path(),
 		c.Get("User-Agent"),
 	)
+
 	return err
 }
