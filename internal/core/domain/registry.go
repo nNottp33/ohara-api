4c6cc54dfc0aca0a237519fd63c9547fe747d0c7
nnottp33 <nnattaponp.dev@gmail.com>
2025-09-28 19:57:01 +0700

    feat: Add domain models for Books, Authors, and Publisher with relationships and base model refactor. Extend registry for model registration and automigration.

--- internal/core/domain/registry.go
+++ internal/core/domain/registry.go
@@ -3,6 +3,7 @@ package domain
 import (
 	"errors"
 	"sync"
+	"time"
 
 	"gorm.io/gorm"
 )
@@ -11,6 +12,13 @@ var (
 	modelRegistry []interface{}
 	registryLock  sync.Mutex
 )
+
+type BaseModel struct {
+	ID        uint           `json:"id" gorm:"primaryKey;autoIncrement"`
+	CreatedAt time.Time      `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time      `json:"updated_at" gorm:"autoUpdateTime"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
+}
 
 func RegisterModel(model interface{}) {
 	registryLock.Lock()
--- internal/core/domain/users.go
+++ internal/core/domain/users.go
@@ -5,21 +5,20 @@ import (
 )
 
 type Users struct {
-	Id             int       `json:"id" gorm:"primaryKey;autoIncrement"`
+	BaseModel
+
 	DisplayName    string    `json:"display_name" gorm:"type:varchar(255);not null"`
-	FirstName      string    `json:"first_name" gorm:"type:varchar(255)"`
+	FirstName      string    `json:"first_name" gorm:"type:varchar(255);index"`
 	LastName       string    `json:"last_name" gorm:"type:varchar(255)"`
 	ImageUrl       string    `json:"image_url" gorm:"type:varchar(255)"`
-	Username       string    `json:"username" gorm:"type:varchar(50);not null"`
-	Password       string    `json:"password" gorm:"type:varchar(255)"`
-	Email          string    `json:"email" gorm:"type:varchar(50);not null"`
+	Username       string    `json:"username" gorm:"type:varchar(50);not null;index"`
+	Password       string    `json:"password" gorm:"type:varchar(255);index"`
+	Email          string    `json:"email" gorm:"type:varchar(50);not null;index"`
 	MobileNumber   string    `json:"mobile_number" gorm:"type:varchar(255);not null"`
 	DialCode       string    `json:"dial_code" gorm:"type:varchar(255);not null"`
 	ProviderSocial string    `json:"provider_social" gorm:"type:varchar(255)"`
-	SocialId       string    `json:"social_id" gorm:"type:varchar(255)"`
+	SocialId       string    `json:"social_id" gorm:"type:varchar(255);index"`
 	LastLoginAt    time.Time `json:"last_login_at"`
-	CreatedAt      time.Time `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt      time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
 
 func (Users) TableName() string {
